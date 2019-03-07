6fc6f56a770272f36620bf1834aa120a3a7bc0f4
aguyon <alexisguyon01@gmail.com>
2019-03-07 15:57:10 +0100

    Section2 - Alexis

--- yoshi-2022.js
+++ yoshi-2022.js
@@ -193,7 +193,27 @@
 
 
 		/*-------------------script-section-2----------------------*/
+		var carousel = $(".carousel"),
+		items = $(".item"),
+		currdeg  = 0;
 
+		$(".next").on("click", { d: "n" }, rotate);
+		$(".prev").on("click", { d: "p" }, rotate);
+
+		function rotate(e){
+			if(e.data.d=="n"){
+				currdeg = currdeg - 60;
+			}
+			if(e.data.d=="p"){
+				currdeg = currdeg + 60;
+			}
+			carousel.css({
+				"transform": "rotateY("+currdeg+"deg)"
+			});
+			items.css({
+				"transform": "rotateY("+(-currdeg)+"deg)"
+			});
+		}
 
 		/*-------------------script-section-3----------------------*/
 
