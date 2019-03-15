ec91a24dad0e84c2da40131762951a122de9d88c
Benoît Briastre <benoit.briastre@gmail.com>
2019-03-15 11:13:58 +0100

    modif BBB

---
+++ bbscript.js
@@ -0,0 +1,23 @@
+anime.timeline({loop: true})
+  .add({
+    targets: '#titremeeting .word',
+    scale: [14,1],
+    opacity: [0,1],
+    easing: "easeOutCirc",
+    duration: 400,
+    delay: function(el, i) {
+      return 800 * i;
+    }
+  }).add({
+    targets: '#titremeeting',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+const mapbutton = document.getElementById("bigmapbutton")
+
+const openbutton = () => {top.location.href="https://www.google.com/maps/d/u/0/viewer?mid=192M6t3LffQyZpKAUdXm01ddGeqgF6y9y&ll=52.80640147441079%2C10.685783677490463&z=5"};
+
+bigmapbutton.addEventListener('click', openbutton);
